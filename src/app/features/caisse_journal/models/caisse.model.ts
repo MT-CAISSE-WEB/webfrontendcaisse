e69525e0f42c93b5b0af8bc1af06c8c43b9f1f64
Ferreol <fefeferreol242@gmail.com>
2026-07-06 11:53:57 +0100

    Changement logo

--- src/app/features/caisse_journal/models/caisse.model.ts
+++ src/app/features/caisse_journal/models/caisse.model.ts
@@ -1,17 +1,30 @@
-import { MainModel } from "../../../_core/models/main.model";
-import { journalModel } from "./journal.model";
+import { MainModel } from '../../../_core/models/main.model';
+import { journalModel } from './journal.model';
 
-export class caisseModel extends MainModel{
-    idcaisse : string = "";
-    codecaisse : string = "";
-    libelle : string = "";
-    journal : journalModel = new journalModel();
-    devise : any;
-    site : any;
-    compte : any;
-    societe :  any;
-    actif : Number = 0;
-    dateinitialisation : string = "";
-    soldeinitialisation : Number = 0;
-    seuilminimal : Number = 0;
+export class caisseModel extends MainModel {
+  idcaisse: string = '';
+  codecaisse: string = '';
+  libelle: string = '';
+  journal: journalModel = new journalModel();
+  devise: any;
+  site: any;
+  compte: any;
+  societe: any;
+  actif: Number = 0;
+  dateinitialisation: string = '';
+  soldeinitialisation: Number = 0;
+  seuilminimal: Number = 0;
+}
+export interface CaisseSolde {
+  idcaisse: string;
+  codecaisse: string;
+  libelle: string;
+  codedevise: string;
+  soldeouverture: number;
+  derniere_date_periode: string;
+  total_encaissement: number;
+  total_encaissement_ref: number;
+  total_decaissement: number;
+  total_decaissement_ref: number;
+  solde_theorique: number;
 }
